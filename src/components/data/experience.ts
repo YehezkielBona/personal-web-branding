1370bc71fbe6653da2badcb06a5753335bd73ec5
Repiticion <kkyehe@gmail.com>
2026-09-05 23:39:26 +0700

    update experience

--- src/components/data/experience.ts
+++ src/components/data/experience.ts
@@ -11,4 +11,15 @@ export const experiences: Experience[] = [
         "Belajar dan menerapkan praktik Git untuk kolaborasi"
     ],
   },
+  {
+    id: "exp-1",
+    jobTitle: "ICT Primary Teacher (Internhsip)",
+    company: "Yayasan Harapan Global Mandiri",
+    duration: "2025 - 2026",
+    responsibilities: [
+        "Mengajar Scratch Dasar Pembuatan Game Gr 5th",
+        "Mengajar Excel Formula, Sheet dan Studi Kasus Gr 6th", 
+        "Mengajar HTML Advanced di Notepad dan Basic CSS Gr 6th"
+    ],
+  },
 ];
